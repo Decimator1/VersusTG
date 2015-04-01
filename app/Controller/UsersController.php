39d4f40e8b63c3215946c14341c879201d8e165b
Andrew Dallosta <dallostaa@gmail.com>
2015-03-31 22:55:36 -0700

    Deleted unnecessary code and started user login section. Also added password hashing to the registration section so that the user passwords would be a lot more secure.

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -41,6 +41,24 @@ class UsersController extends AppController {
 		$this->set('user', $this->User->find('first', $options));
 	}
 
+	public function beforeFilter() {
+	    parent::beforeFilter();
+	    // Allow users to register and logout.
+	    $this->Auth->allow('register', 'logout');
+	}
+
+	public function login() {
+	    if ($this->request->is('post')) {
+	        if ($this->Auth->login()) {
+	            return $this->redirect($this->Auth->redirectUrl());
+	        }
+	        $this->Session->setFlash(__('Invalid username or password, try again'));
+	    }
+	}
+
+	public function logout() {
+	    return $this->redirect($this->Auth->logout());
+	}
 /**
  * add method
  *
